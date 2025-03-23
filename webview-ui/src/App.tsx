b729b94e2540e7b5ca30431a31a320f134bc9759
DarkChan <buihohaidang@gmail.com>
2025-03-23 18:57:04 +0700

    Refactor: Remove unused CSS and update ChatMessage interface for improved data structure

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -2,21 +2,27 @@ import React from 'react'; //, { useEffect, useState, useRef }
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TooltipProvider } from '@/components/ui/tooltip';
 
+// import page
 import Config from '@/components/Config';
-import History from '@/pages/history';
 import Index from '@/pages/Index';
+import History from '@/pages/History';
+
+// import components
+import { ChatProvider } from "@/context/ChatContext";
 
 
 const App: React.FC = () => {
   return (
     <TooltipProvider>
-      <Router>
-        <Routes>
-          <Route path="*" element={<Index />} />
-          <Route path="/config" element={<Config />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </Router>
+      <ChatProvider>
+        <Router>
+          <Routes>
+            <Route path="*" element={<Index />} />
+            <Route path="/config" element={<Config />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </Router>
+      </ChatProvider>
     </TooltipProvider>
   );
 };
